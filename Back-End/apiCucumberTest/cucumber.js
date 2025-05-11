4c4209f460c787c6cdc177aacf56ce1a2258e0ee
marcapiteam <78953366+marcapiteam@users.noreply.github.com>
2025-05-11 20:12:48 +0200

    Delete Back-End/apiCucumberTest directory

--- Back-End/apiCucumberTest/cucumber.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  default: {
-    require: ['steps/**/*.ts'],
-    requireModule: ['ts-node/register'],
-    format: ['progress', 'json:reports/cucumber_report.json'],
-    publishQuiet: true
-  }
-};
